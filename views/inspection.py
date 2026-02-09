ba070a6094ea93c310fc86557c6f82eb94da75b1
domagojbrkovic0505 <domagoj.brkovic0505@gmail.com>
2026-02-09 01:34:10 +0100

    Update inspection.py

--- views/inspection.py
+++ views/inspection.py
@@ -1,5 +1,6 @@
 ﻿from logic.shopfloor_rules import STATIONS, calculate_inspection_load
-from logic.inspection_graph import build_inspection_graph
+from logic.inspection_graph import build_graph_for_route
+
 from ui.inspection_cytoscape import render_inspection_cytoscape
 
 import streamlit as st
@@ -10,7 +11,7 @@ def render_inspection(spools_df):
     st.success("Inspection view loaded")
     inspection_load = calculate_inspection_load(spools_df)
 
-    graph = build_inspection_graph(
+    graph = build_graph_for_route(
         stations=STATIONS,
         inspection_load=inspection_load,
     )
